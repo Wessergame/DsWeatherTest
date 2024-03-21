e6d08da9fd399bd9a0c2fcb4b3062a5c254c8340
Polianin Nikita <nikitkapolyanin@yandex.ru>
2024-03-21 21:11:20 +0300

    feat: add an interface for standard saving changes

---
+++ Application/Interfaces/DbContexts/IDbContextBase.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.DbContexts;
+public interface IDbContextBase
+{
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
