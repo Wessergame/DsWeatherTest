ce12b10636953f9410628378d4f95d757648a114
Polianin Nikita <nikitkapolyanin@yandex.ru>
2024-03-22 00:55:22 +0300

    feat: add DI

---
+++ Persistence/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Application.Interfaces.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+namespace Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connection = configuration["ConnectionStringSql"];
+        services.AddDbContext<UberDbContext>(option => option.UseNpgsql(connection));
+
+        services.AddScoped<IWeatherDbContext>(provider => provider.GetService<UberDbContext>()!);
+        services.AddScoped<IWindDirectionDbContext>(provider => provider.GetService<UberDbContext>()!);
+
+        return services;
+    }
+}
