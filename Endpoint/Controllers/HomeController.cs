5e709a1f96c42ab9e799ff6fa903c90846cc4fb6
Polianin Nikita <nikitkapolyanin@yandex.ru>
2024-03-23 23:19:36 +0300

    feat: add upload page

--- Endpoint/Controllers/HomeController.cs
+++ Endpoint/Controllers/HomeController.cs
@@ -32,6 +32,11 @@ public class HomeController(ILogger<HomeController> logger, IMediator mediator) 
         return View();
     }
 
+    public IActionResult Upload()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
